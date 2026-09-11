15384046d785b94615dfe0b8d3e6db1b7d72c77b
SALVADOR PUIGDEVALL SOSA <57789653@ceibal.edu.uy>
2026-09-11 20:31:22 -0300

    botón desplegable del header

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,36 @@
-document.getElementById("loginButton").addEventListener("click", (e) => {
-    e.preventDefault();
+const loginButton = document.getElementById("loginButton");
+if (loginButton){
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
 
-const UsuarioIngresado = document.getElementById("user").value;
-const PasswordIngresado = document.getElementById("contrasena").value;
+    const UsuarioIngresado = document.getElementById("user").value;
+    const PasswordIngresado = document.getElementById("contrasena").value;
 
 
-if (UsuarioIngresado === "Estudiante" && PasswordIngresado === "Estudiante") {
+    if (UsuarioIngresado === "Estudiante" && PasswordIngresado === "Estudiante") {
 
-    window.location.href = "principal-estudiante.html";
+        window.location.href = "principal-estudiante.html";
+    }
+    else if (UsuarioIngresado === "Docente" && PasswordIngresado === "Docente") {
+        window.location.href = "principal-docente.html";
+    }
+    else{
+        alert("Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.");
+    }
+    });
 }
-else if (UsuarioIngresado === "Docente" && PasswordIngresado === "Docente") {
-    window.location.href = "principal-docente.html";
+
+/*-------------------------------------------*/
+/*Funcionamiento del botón del menú desplegable*/
+
+//Guarda el botón en una constante
+const botonMenu = document.querySelector("#menuHeader button");
+if (botonMenu){
+    //Agrega un evento de "Click" al botón
+    botonMenu.addEventListener("click", () => {
+        //Guarda el <ul> hijo del menú 
+        let menuDesplegable = document.querySelector("#menuHeader ul");
+        //Asigna o quita la clase "oculto" al elemento, lo hace cada vez que se presiona el botón.
+        menuDesplegable.classList.toggle("oculto");
+    });
 }
-else{
-    alert("Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.");
-}
-});
