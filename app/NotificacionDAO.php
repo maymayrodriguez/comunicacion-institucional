09e60b0fa126737bcd7958f50d93b4128a059f33
Mayra Rodríguez <maymayrodriguezco@gmail.com>
2026-09-15 19:32:12 -0300

    PHP con insert y select

--- app/NotificacionDAO.php
+++ app/NotificacionDAO.php
@@ -2,8 +2,22 @@
 
     require_once 'conexion.php';
 
-    $sql = "SELECT * FROM notificaciones";
-    $resultado = $conexion->query($sql);
+    //Código de consulta de prueba
+    $identificador = 13;
+    $nombre = "Notificación de prueba";
+    $descripcion = "Esta es una notificación de prueba";
+    $publico = "3MC";
+    
+    $sqlInsert = "INSERT INTO notificaciones (identificador, nombre, descripcion, publico) VALUES (?, ?, ?, ?)";  
+
+    $sentencia = $conexion->prepare($sqlInsert);
+    $sentencia->bind_param("isss", $identificador, $nombre, $descripcion, $publico);
+    $sentencia ->execute();
+
+
+
+    $sqlSelect = "SELECT * FROM notificaciones";
+    $resultado = $conexion->query($sqlSelect);
 
     while ($fila = $resultado->fetch_assoc()) {
         echo "Identificador: " . $fila["identificador"] . "<br>";
